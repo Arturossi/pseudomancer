a4cbde1e176db04e1b2d48de1ad720b48b51340b
Adam Dinan <adam1989ie@gmail.com>
2024-06-30 20:06:04 +0100

    Remove additional version function

--- pseudomancer/__main__.py
+++ pseudomancer/__main__.py
@@ -64,12 +64,6 @@ def get_options():
 
     return
 
-def print_version(ctx, param, value):
-    if not value or ctx.resilient_parsing:
-        return
-    click.echo('MGEfinder version: ' + __version__)
-    ctx.exit()
-
 def main():
     args = get_options(sys.argv[1:])
 
