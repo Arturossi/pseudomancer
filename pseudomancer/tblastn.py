93312539abadd48058d7d0339194748fc95361f1
Adam Dinan <adam1989ie@gmail.com>
2024-07-01 20:53:49 +0100

    Add initial tblastn version testing

---
+++ pseudomancer/tblastn.py
@@ -0,0 +1,27 @@
+import subprocess
+
+
+def check_blast_version(blast_path: str = "tblastn"):
+    """
+    Check the version of BLAST+ installed on the system.
+
+    Parameters
+    ----------
+    blast_path : str
+        Path to the BLAST+ executable. If not provided, the function will
+        search the system PATH for the executable.
+    """
+    try:
+        blast_version = subprocess.check_output(
+            [blast_path, "-version"],
+            stderr=subprocess.STDOUT,
+            shell=True,
+        )
+    except subprocess.CalledProcessError:
+        sys.stderr.write(
+            "Could not find BLAST+ executable. Please ensure that BLAST+ is"
+            + " installed and in your PATH."
+        )
+        sys.exit(1)
+
+    return blast_version
