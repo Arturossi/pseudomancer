39aa1d75328a7ad46e8a20664e9c25e5a4aa7c19
Adam Dinan <adam1989ie@gmail.com>
2024-07-01 21:04:31 +0100

    Add version parsing to setup and import required modules

--- setup.py
+++ setup.py
@@ -1,17 +1,40 @@
 from setuptools import find_packages, setup
+import io
+import os
+import re
 
-with open(path.join(this_directory, "requirements.txt"), encoding="utf-8") as f:
+with open(
+    os.path.join(this_directory, "requirements.txt"), encoding="utf-8"
+) as f:
     requirements = f.read().splitlines()
 
 
 def readme():
-    with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
         return f.read()
+
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name="pseudomancer",
-    version="0.0.1",
+    version=find_version("pseudomancer/__init__.py"),
     description="pseudomancer: a command line tool for reconstructing "
     + "pseudogenes in prokaryotic genomes",
     long_description=readme(),
